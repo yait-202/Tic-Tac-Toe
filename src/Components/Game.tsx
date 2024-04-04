dcfb55807e494a39bf47654678eba9115548c329
Artem <artemgellyer@gmail.com>
2024-04-05 00:40:32 +0300

    Addded display of 'X' or '0' on tiles

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,19 +1,28 @@
 // Game.tsx
-import React, { useState } from 'react';
-import TileGrid from './TileGrid';
+import React, { useState } from "react";
+import TileGrid from "./TileGrid";
 
 const Game: React.FC = () => {
   const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
-  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [currentPlayer, setCurrentPlayer] = useState("X");
 
   const handleTileClick = (index: number) => {
-    
+    if (board[index] === null) {
+      const newBoard = [...board];
+      newBoard[index] = currentPlayer;
+      setBoard(newBoard);
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    }
   };
 
   return (
     <div className="game">
       <h1>Tic Tac Toe</h1>
-      <TileGrid board={board} currentPlayer={currentPlayer} onTileClick={handleTileClick} />
+      <TileGrid
+        board={board}
+        currentPlayer={currentPlayer}
+        onTileClick={handleTileClick}
+      />
     </div>
   );
 };
--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -7,26 +7,25 @@ interface TileProps {
   handleTileClick: () => void;
 }
 
-export const Tile: React.FC<TileProps> = ({value, handleTileClick}) => {
-
+export const Tile: React.FC<TileProps> = ({ value, handleTileClick }) => {
   return (
     <div>
       <Button
-      onClick={handleTileClick}
-      bg="white"
-      h="120px"
-      w="120px"
-      border="4px"
-      borderColor="slategray"
-      color="black"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-      _hover={{ bg: 'gray.100' }} // Change background color on hover
-    >
-    
-    </Button>
+        onClick={handleTileClick}
+        bg="white"
+        h="120px"
+        w="120px"
+        border="4px"
+        borderColor="slategray"
+        color="black"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        _hover={{ bg: "gray.100" }} // Change background color on hover
+      >
+        {value}
+      </Button>
     </div>
   );
 };
